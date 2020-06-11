ecec5c7e02f8d2af4d2d5615c7da5092fe6a9ffc
giuliobosco <giuliobva@gmail.com>
2020-06-11 17:15:59 +0200

    add util

---
+++ app/src/main/java/ch/giuliobosco/todoappandroid/util/Extensions.kt
@@ -0,0 +1,8 @@
+package ch.giuliobosco.todoappandroid.util
+
+import com.google.android.material.snackbar.Snackbar
+
+fun Snackbar.color(color: Int):Snackbar {
+    view.setBackgroundColor(color)
+    return this
+}
